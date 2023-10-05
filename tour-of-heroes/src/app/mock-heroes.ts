7d45d040b121b83471c4af4e93a6b6f42465b1db
Bernadette Christiner <bernadettechristiner@ac-hp.on-norm.at>
2023-10-05 17:00:21 +0200

    implement heroes detail and routing

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from "./heroes";
+
+export const HEROES: Hero[] = [
+  {id:11, name:"Sandor"},
+  {id:12, name:"Gerhard"},
+  {id:13, name:"Binh"},
+  {id:14, name:"Andreas"},
+  {id:15, name:"Berni"}
+]
